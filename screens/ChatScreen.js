f70ee28d28192755d7a16005ac42de1deeb47dd6
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-10-27 20:11:52 +0530

    chat screen formatting done

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+// import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import colors from '../components/colors'
+import axios from 'axios'
 
 const ChatScreen = () => {
+  const navigation = useNavigation()
+  useLayoutEffect(
+    () => {
+      navigation.setOptions({
+        headerStyle: {
+          backgroundColor: colors.DARK,
+        },
+        headerTintColor: colors.LIGHT,
+        headerTitle: 'Chat'
+      })
+    }, []
+  )
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.container}>
+        <Text>ChatScreen</Text>
+      </View>
+    </ScrollView>
   )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.DARK,
+    flex: 1
+  }
+})
